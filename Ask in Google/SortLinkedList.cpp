ad659b6e017fb8b76dca01cb057c4f1ae167d0e0
vishnu510 <vpal6044@gmail.com>
2022-07-03 14:55:31 +0530

    Sort Linked List

---
+++ Ask in Google/SortLinkedList.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+    public:
+    int val;
+    ListNode* next;
+    ListNode(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+
+void insertion_at_Tail(ListNode*& head, int val) {
+    if (head == NULL) {
+        ListNode* node = new ListNode(val);
+        head = node;
+        return;
+    }
+    ListNode* temp = head;
+    ListNode* node = new ListNode(val);
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = node;
+}
+void print_ll(ListNode* &head){
+  ListNode* temp = head;
+  while(temp){
+      cout<<temp->val<<"->";
+      temp = temp->next;
+  }
+
+}
+
+ListNode* sortList(ListNode* l1){
+    ListNode* p1 = l1,*p2=l1;
+    vector<int> ans;
+    while(p1!=NULL){
+        ans.push_back(p1->val);
+        p1 = p1->next;
+    }
+    sort(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++){
+        p2->val = ans[i];
+        p2 = p2->next;
+    }
+    return l1;
+}
+
+int main(){
+
+    ListNode* l1 = NULL;
+    insertion_at_Tail(l1,1);
+    insertion_at_Tail(l1,2);
+    insertion_at_Tail(l1,8);
+    insertion_at_Tail(l1,5);
+    insertion_at_Tail(l1,7);
+    insertion_at_Tail(l1,9);
+    ListNode* ptr1 = sortList(l1);
+    print_ll(ptr1);  
+    return 0;
+}

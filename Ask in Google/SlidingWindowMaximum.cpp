70771b2772bbb0ae74d982fc788b2367ff34ca57
vishnu510 <vpal6044@gmail.com>
2022-07-06 18:25:26 +0530

    Sliding Window maximum

--- Ask in Google/SlidingWindowMaximum.cpp
+++ Ask in Google/SlidingWindowMaximum.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute force
+
+vector<int> maxSlidingWindow1(vector<int>& nums, int k) {
+        vector<int> res;
+        for(int itr = 0; itr <= nums.size()-k; itr++){
+            int maxWindowValue = INT_MIN;
+            for(int jtr = itr; jtr < itr+k; jtr++){
+                maxWindowValue = max(maxWindowValue, nums[jtr]);
+            }
+            res.push_back(maxWindowValue);
+        }
+        return res;
+}
+
 vector<int> maxSlidingWindow(vector<int>& nums, int k){
     deque<int> dq;
     vector<int> ans;
@@ -25,6 +39,11 @@ int main(){
     vector<int> nums ={1,3,-1,-3,5,3,6,7};
     vector<int> ans = maxSlidingWindow(nums,3);
     for(auto it:ans){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    vector<int> ans1 = maxSlidingWindow1(nums,3);
+    for(auto it:ans1){
         cout<<it<<" ";
     }
     return 0;

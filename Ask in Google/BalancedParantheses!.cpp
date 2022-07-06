3f7e494a6d1580145a6cf2b5d0a208c35fb60659
vishnu510 <vpal6044@gmail.com>
2022-07-06 18:44:43 +0530

    Valid Paranthesis

---
+++ Ask in Google/BalancedParantheses!.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s){
+    stack<char> stk;
+    for(auto i:s){
+        if(i=='(' || i=='{' || i=='['){
+            stk.push(i);
+        }
+        else{
+            if(stk.empty() || stk.top()=='('&&i!=')' || stk.top()=='{'&&i!='}' || stk.top()=='['&&i!=']'){
+            return false;
+            }
+            stk.pop();
+        }
+    }
+return stk.empty();
+}
+
+int main(){
+
+    string str ="()[]{}";
+    cout<<isValid(str);
+    return 0;
+}

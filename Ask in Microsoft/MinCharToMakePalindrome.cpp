b6aa671655059d7e644f81c6d976d8dae5fbc9a7
vishnu510 <vpal6044@gmail.com>
2022-08-01 20:23:55 +0530

    Min Char Require to Make Palindrome

---
+++ Ask in Microsoft/MinCharToMakePalindrome.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int lps(string pat){
+
+    int M = pat.size();
+
+    int lps[M];
+
+    
+
+    int len = 0;
+
+    int i = 1;
+
+    lps[0] = 0;
+
+    
+
+    while(i < M){
+
+        if(pat[i] == pat[len]){
+
+            len++;
+
+            lps[i] = len;
+
+            i++;
+
+        }else{
+
+            if(len!=0){
+
+                len = lps[len-1];
+
+            }else{
+
+                lps[i] = 0;
+
+                i++;
+
+            }
+
+        }
+
+    }
+
+    
+
+    return lps[M-1];
+
+}
+
+int minInsertions(string A){
+
+   string B =A;
+
+   reverse(B.begin(),B.end());
+
+   string str = A+"$"+B;
+
+   int len = lps(str);
+
+   return A.size() - len;
+}
+
+int main(){
+
+    cout<<minInsertions("ABC");
+    return 0;
+}

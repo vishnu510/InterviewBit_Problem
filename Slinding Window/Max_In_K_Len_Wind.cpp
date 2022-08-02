33d3ecd534014bb1611c1d85724eb4ec10091b83
vishnu510 <vpal6044@gmail.com>
2022-08-02 18:31:46 +0530

    Find max in window of Length k

--- Slinding Window/Max_In_K_Len_Wind.cpp
+++ Slinding Window/Max_In_K_Len_Wind.cpp
@@ -1,32 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int  findMax(vector<int>& nums,int k){
+vector<int>  MaxOfWindowK(vector<int>& nums,int k){
 
     int n = nums.size();
-
-    if(n<k) return -1;
-
-    int res = 0,i=0;
-    while(i<k){
-        res += nums[i++];
+    vector<int> ans;
+    if(k>n){
+        ans.push_back(*max_element(nums.begin(),nums.end()));
+        return ans;
     }
 
-    int currSum = res;
+    int i =0, j=0;
+    deque<int> q;
 
-    for(int i=k;i<n;i++){
+    while (j < n)
+    {
+        while (!q.empty() && q.back() < nums[j])
+        {
+            q.pop_back();
+        }
+        q.push_back(nums[j]);
+        
+        if(j-i+1 < k){
+            j++;
+        }
+        else{
 
-        currSum += nums[i] - nums[i-k];
+            ans.push_back(q.front());
 
-        res = max(res,currSum);
+            if(nums[i]==q.front()){
+                q.pop_front();
+            }
+            i++;
+            j++;
+        }
     }
-    return res;
+    return ans;
 }
 
 int main(){
 
-    vector<int> nums = {1,-4,2,8,6,-2,5};
+    vector<int> nums = {1,-4,-2,8,6,-2,5};
 
-    cout<<findMax(nums,8);
+    vector<int> ans = MaxOfWindowK(nums,3);
+
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
     return 0;
 }
